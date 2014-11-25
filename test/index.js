8dd5c5074a39896331f1e8b9fbeded61d81d2917
John Brett <johnbrett7@gmail.com>
2014-11-25 11:37:43 +0000

    changed API for calibrate

--- test/index.js
+++ test/index.js
@@ -10,28 +10,22 @@ var after = lab.after;
 var describe = lab.describe;
 var it = lab.it;
 
-describe('Calibrate', function () {
+var sampleError = JSON.stringify(Boom.badImplementation().output);
+var sampleErrorWithMessage = JSON.stringify(Boom.badImplementation('sample message').output);
+var boomNotFound = JSON.stringify(Boom.notFound().output);
+var boomNotFoundWithMessage = JSON.stringify(Boom.notFound("The resource with that ID does not exist or has already been deleted.").output);
+var validOutput = JSON.stringify({
+  statusCode: 200,
+  data: {
+    'message': 'test'
+  },
+  meta: {}
+});
 
-  var sampleError = JSON.stringify(Boom.badImplementation().output);
-  var sampleErrorWithMessage = JSON.stringify(Boom.badImplementation('sample message').output);
-  var boomNotFound = JSON.stringify(Boom.notFound().output);
-  var boomNotFoundWithMessage = JSON.stringify(Boom.notFound("The resource with that ID does not exist or has already been deleted.").output);
-  var validOutput = JSON.stringify({
-    statusCode: 200,
-    data: {
-      'message': 'test'
-    },
-    meta: {}
-  });
+describe('Calibrate', function () {
 
   it('returns a function when reuired', function(done) {
     expect(typeof Calibrate === 'function');
-    done();
-  });
-
-  it('handles errors', function(done) {
-    var response = Calibrate(new Error, null)
-    expect(JSON.stringify(response.output)).to.equal(sampleError)
     done();
   });
 
@@ -41,31 +35,25 @@ describe('Calibrate', function () {
     done();
   });
 
-  it('wraps non boom errors', function(done) {
-    var response = Calibrate(new Error('message'), null);
-    expect(JSON.stringify(response.output)).to.equal(sampleErrorWithMessage);
-    done();
-  });
-
   it('formats valid responses', function(done) {
-    expect(JSON.stringify(Calibrate(null, {'message': 'test'}))).to.equal(validOutput);
+    expect(JSON.stringify(Calibrate({'message': 'test'}))).to.equal(validOutput);
     done()
   })
 
   it('returns not found for null data', function(done) {
-    expect(JSON.stringify(Calibrate(null, null).output)).to.equal(boomNotFoundWithMessage);
+    expect(JSON.stringify(Calibrate(null).output)).to.equal(boomNotFoundWithMessage);
     done()
   })
 
   it('accepts a default return string', function(done) {
-    expect(JSON.stringify(Calibrate(null, null, null, {return_string: 'test'}).output))
+    expect(JSON.stringify(Calibrate(null, null, {return_string: 'test'}).output))
     .to
     .equal(JSON.stringify(Boom.notFound('test').output));
     done()
   })
 
   it('accepts a default context for not found errors', function(done) {
-    expect(JSON.stringify(Calibrate(null, null, null, {context: 'user'}).output))
+    expect(JSON.stringify(Calibrate(null, null, {context: 'user'}).output))
     .to
     .equal(JSON.stringify(Boom.notFound('The user resource with that ID does not exist or has already been deleted.').output));
     done()
@@ -83,7 +71,7 @@ describe('Calibrate', function () {
       }
     });
 
-    expect(JSON.stringify(Calibrate(null, { message: 'test'}, {items: 1 }))).to.equal(expected_response);
+    expect(JSON.stringify(Calibrate({ message: 'test'}, {items: 1 }))).to.equal(expected_response);
     done()
   })
 
@@ -99,7 +87,7 @@ describe('Calibrate', function () {
       }
     });
 
-    expect(JSON.stringify(Calibrate(null, { message: 'test'}, {items: 1 }, {}))).to.equal(expected_response);
+    expect(JSON.stringify(Calibrate({ message: 'test'}, {items: 1 }, {}))).to.equal(expected_response);
     done()
   })
 
