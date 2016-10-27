62f5ed1275e1343075f94c5fe99069722e9747e5
Prashant Katwa <pkatwa@gmail.com>
2016-10-27 01:46:10 -0400

    Update decorator method to accept meta and options
    Currently `Calibrate.response` allows for 3 parameters for data, meta and options. However using the decorator method does not allow for the additional information (meta and options). Is there a reason that its not included? This PR updates the method to accept those params.

--- decorate.js
+++ decorate.js
@@ -4,9 +4,9 @@ const Calibrate = require('./index');
 
 module.exports = function (server, options, next) {
 
-    const decorator = function (res){
+    const decorator = function (res, meta, options){
 
-        return this.response(Calibrate(res));
+        return this.response(Calibrate(res, meta, options));
     };
     server.decorate('reply', 'calibrate', decorator);
 
