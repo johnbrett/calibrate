7df70939b8f5afcf5047ebb42f907a563a176c7a
Esteban Primost <esteban.primost@gmail.com>
2018-01-25 01:19:34 -0300

    Updated tests

--- test/index.js
+++ test/index.js
@@ -11,9 +11,9 @@ const expect = Code.expect;
 const describe = lab.describe;
 const it = lab.it;
 
-const sampleError = JSON.stringify(Boom.badImplementation().output);
-const boomNotFound = JSON.stringify(Boom.notFound().output);
-const boomNotFoundWithMessage = JSON.stringify(Boom.notFound('The resource with that ID does not exist or has already been deleted.').output);
+const sampleError = JSON.stringify(Boom.badImplementation().output.payload);
+const boomNotFound = JSON.stringify(Boom.notFound().output.payload);
+const boomNotFoundWithMessage = JSON.stringify(Boom.notFound('The resource with that ID does not exist or has already been deleted.').output.payload);
 const validOutput = JSON.stringify({
     statusCode: 200,
     data: {
@@ -24,63 +24,55 @@ const validOutput = JSON.stringify({
 
 describe('Calibrate', () => {
 
-    it('returns a function when reuired', (done) => {
+    it('returns a function when reuired', () => {
 
         expect(typeof Calibrate).to.equal('function');
-        done();
     });
 
-    it('wraps non-boom errors', (done) => {
+    it('wraps non-boom errors', () => {
 
         const response = Calibrate(new Error());
-        expect(JSON.stringify(response.output)).to.equal(sampleError);
-        done();
+        expect(JSON.stringify(response.output.payload)).to.equal(sampleError);
     });
 
-    it('doesn\'t wrap boom errors', (done) => {
+    it('doesn\'t wrap boom errors', () => {
 
         const response = Calibrate(Boom.notFound());
-        expect(JSON.stringify(response.output)).to.equal(boomNotFound);
-        done();
+        expect(JSON.stringify(response.output.payload)).to.equal(boomNotFound);
     });
 
-    it('formats valid responses', (done) => {
+    it('formats valid responses', () => {
 
         expect(JSON.stringify(Calibrate({ 'message': 'test' }))).to.equal(validOutput);
-        done();
     });
 
-    it('returns not found for null data', (done) => {
+    it('returns not found for null data', () => {
 
-        expect(JSON.stringify(Calibrate(null).output)).to.equal(boomNotFoundWithMessage);
-        done();
+        expect(JSON.stringify(Calibrate(null).output.payload)).to.equal(boomNotFoundWithMessage);
     });
 
-    it('accepts a default return string', (done) => {
+    it('accepts a default return string', () => {
 
         expect(JSON.stringify(Calibrate(null, null, { 'return_string': 'test' }).output))
-        .to
-        .equal(JSON.stringify(Boom.notFound('test').output));
-        done();
+            .to
+            .equal(JSON.stringify(Boom.notFound('test').output));
     });
 
-    it('accepts a default return string in camelcase', (done) => {
+    it('accepts a default return string in camelcase', () => {
 
         expect(JSON.stringify(Calibrate(null, null, { returnString: 'test' }).output))
-        .to
-        .equal(JSON.stringify(Boom.notFound('test').output));
-        done();
+            .to
+            .equal(JSON.stringify(Boom.notFound('test').output));
     });
 
-    it('accepts a default context for not found errors', (done) => {
+    it('accepts a default context for not found errors', () => {
 
         expect(JSON.stringify(Calibrate(null, null, { context: 'user' }).output))
-        .to
-        .equal(JSON.stringify(Boom.notFound('The user resource with that ID does not exist or has already been deleted.').output));
-        done();
+            .to
+            .equal(JSON.stringify(Boom.notFound('The user resource with that ID does not exist or has already been deleted.').output));
     });
 
-    it('adds meta if defined', (done) => {
+    it('adds meta if defined', () => {
 
         const expectedResponse = JSON.stringify({
             statusCode: 200,
@@ -93,10 +85,9 @@ describe('Calibrate', () => {
         });
 
         expect(JSON.stringify(Calibrate({ message: 'test' }, { items: 1 }))).to.equal(expectedResponse);
-        done();
     });
 
-    it('adds accepts an object parameter', (done) => {
+    it('adds accepts an object parameter', () => {
 
         const expectedResponse = JSON.stringify({
             statusCode: 200,
@@ -109,34 +100,79 @@ describe('Calibrate', () => {
         });
 
         expect(JSON.stringify(Calibrate({ message: 'test' }, { items: 1 }, {}))).to.equal(expectedResponse);
-        done();
     });
 
-    it('decorates the hapi reply interface', (done) => {
+    it('decorates the hapi reply interface', async () => {
 
         const server = new Hapi.Server();
-        server.connection();
-        server.register({
-            register: Calibrate.decorate
-        }, () => {
 
-            server.route({
-                method: 'GET',
-                path: '/',
-                config: {
-                    handler: function (response, reply) {
-
-                        reply.calibrate({ message: 'test' });
-                    }
-                }
-            });
-
-            server.inject({ method: 'GET', url: '/' }, (res) => {
-
-                expect(res.payload).to.equal(validOutput);
-                done();
-            });
+        await server.register({
+            plugin: Calibrate.hapi,
+            options: { onResponse: false }
         });
+
+        server.route({
+            method: 'GET',
+            path: '/',
+            config: {
+                handler: function (request, h) {
+
+                    return h.calibrate({ message: 'test' });
+                }
+            }
+        });
+
+        const res = await server.inject({ method: 'GET', url: '/' });
+
+        expect(res.payload).to.equal(validOutput);
+    });
+
+    it('adds default calibrate wrapper to response', async () => {
+
+        const server = new Hapi.Server();
+
+        await server.register({
+            plugin: Calibrate.hapi
+        });
+
+        server.route({
+            method: 'GET',
+            path: '/',
+            config: {
+                handler: function (request, h) {
+
+                    return { message: 'test' };
+                }
+            }
+        });
+
+        const res = await server.inject({ method: 'GET', url: '/' });
+
+        expect(res.payload).to.equal(validOutput);
+    });
+
+    it('should return response error', async () => {
+
+        const server = new Hapi.Server();
+
+        await server.register({
+            plugin: Calibrate.hapi
+        });
+
+        server.route({
+            method: 'GET',
+            path: '/',
+            config: {
+                handler: function (request, h) {
+
+                    return new Error();
+                }
+            }
+        });
+
+        const res = await server.inject({ method: 'GET', url: '/' });
+
+        expect(res.payload).to.equal(sampleError);
     });
 
 });
